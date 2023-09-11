5b4f8aad4524e5cd284ce5fb6eadf7b4be897682
awpozdnyakov <102249897+awpozdnyakov@users.noreply.github.com>
2023-09-12 02:54:15 +0700

    Add project files

---
+++ FollowNature/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FollowNature
+//
+//  Created by Наталья Рябкова on 12.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
