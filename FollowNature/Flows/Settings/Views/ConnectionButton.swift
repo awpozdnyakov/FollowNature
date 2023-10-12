0bd03dd92a10a9e425958201895eb5e3368cedcb
riitterz <gidirimr@gmail.com>
2023-10-12 15:16:06 -0400

    [IOS-010] Add SettingsScreenView with links to connect developers.

---
+++ FollowNature/Flows/Settings/Views/ConnectionButton.swift
@@ -0,0 +1,40 @@
+//
+//  ConnectionButton.swift
+//  FollowNature
+//
+//  Created by Macbook on 09.10.2023.
+//
+
+import SwiftUI
+
+struct ConnectionButton: View {
+    
+    @Binding var openDevelopersTable: Bool
+    
+    init(openDevelopersTable: Binding<Bool>) {
+        self._openDevelopersTable = openDevelopersTable
+    }
+
+    var body: some View {
+        VStack {
+            Button {
+                withAnimation {
+                    openDevelopersTable = true
+                }
+            } label: {
+                HStack(spacing: 23) {
+                    Image(systemName: "ellipsis.message")
+                        .resizable()
+                        .frame(width: 40, height: 37)
+                        .foregroundColor(Asset.Colors.green.swiftUIColor)
+                    Text(L10n.connection)
+                        .foregroundColor(.black)
+                        .font(.system(size: 20, weight: .medium))
+                }
+                .padding(.leading, 20)
+                .padding(.vertical, 10)
+                .frame(width: 400, alignment: .leading)
+            }
+        }
+    }
+}
