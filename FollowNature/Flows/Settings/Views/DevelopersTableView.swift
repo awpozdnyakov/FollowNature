bb02f1e76770e08e1503bac9659ef6594d71e85b
Andrey Pozdnyakov <awpozdnyakov@gmail.com>
2023-10-15 04:18:16 +0700

    [IOS-012] Finished PhotoPicker & design correction in SettingsScreenView

--- FollowNature/Flows/Settings/Views/DevelopersTableView.swift
+++ FollowNature/Flows/Settings/Views/DevelopersTableView.swift
@@ -10,65 +10,69 @@ import SwiftUI
 struct DevelopersTableView: View {
     
     var body: some View {
+        VisualEffectView(effect: UIBlurEffect(style: .light))
+            .edgesIgnoringSafeArea(.all)
         ZStack {
-            VisualEffectView(effect: UIBlurEffect(style: .light))
-                .edgesIgnoringSafeArea(.all)
-            ZStack {
-                Rectangle()
-                    .foregroundColor(.white)
-                    .overlay(RoundedRectangle(cornerRadius: 25).stroke(Asset.Colors.greenLight.swiftUIColor, lineWidth: 2))
-                VStack {
-                    Link(destination: URL(string: L10n.urlAndrew)!) {
-                        HStack {
-                            Text(L10n.andrew)
-                                .foregroundColor(Asset.Colors.green.swiftUIColor)
-                                .font(.system(size: 26, weight: .medium))
-                        }
-                        .padding(.leading, 20)
-                        .frame(width: 400, height: 50, alignment: .leading)
+            Rectangle()
+                .foregroundColor(.white)
+                .overlay(RoundedRectangle(cornerRadius: 25).stroke(Asset.Colors.greenLight.swiftUIColor, lineWidth: 2))
+            VStack {
+                Link(destination: URL(string: L10n.urlAndrew)!) {
+                    HStack {
+                        Text(L10n.andrew)
+                            .foregroundColor(Asset.Colors.green.swiftUIColor)
+                            .font(.system(size: 26, weight: .medium))
+                        Spacer()
                     }
-                    Divider()
-                        .frame(height: 1)
-                        .overlay(Asset.Colors.greenLight.swiftUIColor)
-                    Link(destination: URL(string: L10n.urlShamil)!) {
-                        HStack {
-                            Text(L10n.shamil)
-                                .foregroundColor(Asset.Colors.green.swiftUIColor)
-                                .font(.system(size: 26, weight: .medium))
-                        }
-                        .padding(.leading, 20)
-                        .frame(width: 400, height: 50, alignment: .leading)
+                    .padding(.leading, 20)
+                    .frame(height: 50, alignment: .leading)
+                }
+                Divider()
+                    .frame(height: 1)
+                    .overlay(Asset.Colors.greenLight.swiftUIColor)
+                Link(destination: URL(string: L10n.urlShamil)!) {
+                    HStack {
+                        Text(L10n.shamil)
+                            .foregroundColor(Asset.Colors.green.swiftUIColor)
+                            .font(.system(size: 26, weight: .medium))
+                        Spacer()
                     }
-                    Divider()
-                        .frame(height: 1)
-                        .overlay(Asset.Colors.greenLight.swiftUIColor)
-                    Link(destination: URL(string: L10n.urlTilek)!) {
-                        HStack {
-                            Text(L10n.tilek)
-                                .foregroundColor(Asset.Colors.green.swiftUIColor)
-                                .font(.system(size: 26, weight: .medium))
-                        }
-                        .padding(.leading, 20)
-                        .frame(width: 400, height: 50, alignment: .leading)
+                    .padding(.leading, 20)
+                    .frame(height: 50, alignment: .leading)
+                }
+                Divider()
+                    .frame(height: 1)
+                    .overlay(Asset.Colors.greenLight.swiftUIColor)
+                Link(destination: URL(string: L10n.urlTilek)!) {
+                    HStack {
+                        Text(L10n.tilek)
+                            .foregroundColor(Asset.Colors.green.swiftUIColor)
+                            .font(.system(size: 26, weight: .medium))
+                        Spacer()
                     }
-                    Divider()
-                        .frame(height: 1)
-                        .overlay(Asset.Colors.greenLight.swiftUIColor)
-                    Link(destination: URL(string: L10n.urlRehina)!) {
-                        HStack {
-                            Text(L10n.rehina)
-                                .foregroundColor(Asset.Colors.green.swiftUIColor)
-                                .font(.system(size: 26, weight: .medium))
-                        }
-                        .padding(.leading, 20)
-                        .frame(width: 400, height: 50, alignment: .leading)
+                    .padding(.leading, 20)
+                    .frame(height: 50, alignment: .leading)
+                }
+                Divider()
+                    .frame(height: 1)
+                    .overlay(Asset.Colors.greenLight.swiftUIColor)
+                Link(destination: URL(string: L10n.urlRehina)!) {
+                    HStack {
+                        Text(L10n.rehina)
+                            .foregroundColor(Asset.Colors.green.swiftUIColor)
+                            .font(.system(size: 26, weight: .medium))
+                        Spacer()
                     }
+                    .padding(.leading, 20)
+                    .frame(height: 50, alignment: .leading)
                 }
             }
-            .frame(width: 400, height: 280)
-            .cornerRadius(25)
-            .padding(.bottom, 147)
         }
+        .padding(.horizontal, 15)
+        .frame(maxWidth: .infinity)
+        .frame(height: 280)
+        .cornerRadius(25)
+        .padding(.bottom, 147)
     }
 }
 
